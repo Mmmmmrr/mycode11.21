7d6e417c86e2ae76d577b88e9f588cbd546bed86
Mmmmmrr <1539716914@qq.com>
2019-05-27 17:35:14 +0800

    斐波那契数、括号匹配

--- OJ/OJ/test.cpp
+++ OJ/OJ/test.cpp
@@ -95,13 +95,96 @@ using namespace std;
 //}
 
 //µ¹ÖÃ×Ö·û´®
+//int main()
+//{
+//	string s1, s2;
+//	cin >> s2;
+//	while (cin >> s1)
+//		s2 = s1 + " " + s2;
+//	cout << s2 << endl;
+//	return 0;
+//}
+
+//·ÅÖÃµ°¸â
+//#include <iostream>
+//#include <vector>
+//using namespace std;
+//int main()
+//{
+//	int w, h, ret = 0;
+//	cin >> w >> h;
+//	vector<vector<int>> v;
+//	v.resize(w);
+//	for (int i = 0; i < v.size(); i++)
+//	{
+//		v[i].resize(h, 1);
+//	}
+//	for (int i = 0; i < w; i++)
+//	{
+//		for (int j = 0; j < h; j++)
+//		{
+//			if (v[i][j] == 1)
+//			{
+//				ret++;
+//				if ((i + 2) <w)
+//				{
+//					v[i + 2][j] = 0;
+//				}
+//				if ((j + 2) <h)
+//				{
+//					v[i][j + 2] = 0;
+//				}
+//			}
+//		}
+//	}
+//	cout << ret;
+//	return 0;
+//}
+
+//À¨ºÅÆ¥Åä
+//class Parenthesis {
+//public:
+//	bool chkParenthesis(string A, int n) {
+//		// write code here
+//		int count = 0;
+//		for (int i = 0; i < n; ++i){
+//			if (A[i] == '(')
+//				count++;
+//			else if (A[i] == ')')
+//				count--;
+//			else
+//				return false;
+//			if (count < 0)
+//				return false;
+//		}
+//		return true;
+//	}
+//};
+
+//ì³²¨ÄÇÆõÊý
+#include <iostream>
+#include <vector>
+using namespace std;
+int N = 1000000;
+int f[1000000];
 int main()
 {
-	string s1, s2;
-	cin >> s2;
-	while (cin >> s1)
-		s2 = s1 + " " + s2;
-	cout << s2 << endl;
-	return 0;
-}
 
+	f[0] = 1;
+	f[1] = 1;
+	for (int i = 2; i < N; i++)
+	{
+		f[i] = f[i - 1] + f[i - 2];
+	}
+	int n;
+	cin >> n;
+	int i = 0;
+	for (; i < N - 1; i++)
+	{
+		if (f[i] <= n && f[i + 1] >= n)
+			break;
+	}
+	int ret = min(n - f[i], f[i + 1] - n);
+	cout << ret;
+
+}
