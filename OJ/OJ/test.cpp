20a90f04b2979a5fdf80476788629ec6e1584eb6
Mmmmmrr <1539716914@qq.com>
2019-05-30 17:10:37 +0800

    井字棋、密码强度

--- OJ/OJ/test.cpp
+++ OJ/OJ/test.cpp
@@ -374,3 +374,105 @@ public:
 		return max;
 	}
 };
+
+//ÃÜÂëÇ¿¶È
+#include <iostream>
+#include <string>
+using namespace std;
+
+int length(string s)
+{
+	int res;
+	int n = s.size();
+	if (n <= 4) res = 5;
+	else if (n <= 7) res = 10;
+	else res = 25;
+	return res;
+}
+int Abc(string s)
+{
+	int res, res1 = 0, res2 = 0;
+	int n = s.size();
+	for (int i = 0; i<n; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z') res1++;
+		else if (s[i] >= 'A' && s[i] <= 'Z') res2++;
+	}
+	if (res1 == 0 && res2 == 0) res = 0;
+	else if (res1 == 0 || res2 == 0) res = 10;
+	else res = 20;
+	return res;
+}
+
+int num(string s)
+{
+	int res, res1 = 0;
+	int n = s.size();
+	for (int i = 0; i<n; i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9') res1++;
+	}
+	if (res1 == 0) res = 0;
+	else if (res1 == 1) res = 10;
+	else res = 20;
+	return res;
+}
+
+int chars(string s)
+{
+	int res, res1 = 0;
+	int n = s.size();
+	for (int i = 0; i<n; i++)
+	{
+		if ((s[i] >= 33 && s[i] <= 47) || (s[i] >= 58 && s[i] <= 64) || (s[i] >= 91 && s[i] <= 96) || (s[i] >= 123 && s[i] <= 126))
+			res1++;
+	}
+	if (res1 == 0) res = 0;
+	else if (res1 == 1) res = 10;
+	else res = 25;
+	return res;
+}
+int main()
+{
+	string s;
+	while (getline(cin, s))
+	{
+		int res = length(s) + Abc(s) + num(s) + chars(s);
+		if ((Abc(s)>10) && num(s) && chars(s)) res += 5;
+		else if (Abc(s) && num(s) && chars(s)) res += 3;
+		else if (Abc(s) && num(s)) res += 2;
+
+		if (res >= 90) cout << "VERY_SECURE" << endl;
+		else if (res >= 80) cout << "SECURE" << endl;
+		else if (res >= 70) cout << "VERY_STRONG" << endl;
+		else if (res >= 60) cout << "STRONG" << endl;
+		else if (res >= 50) cout << "AVERAGE" << endl;
+		else if (res >= 25) cout << "WEAK" << endl;
+		else cout << "VERY_WEAK" << endl;
+	}
+
+	return 0;
+}
+//¾®×ÖÆå
+class Board {
+public:
+	bool checkWon(vector<vector<int> > board) {
+		// write code here
+		for (int i = 0; i < 3; i++)
+		{
+			if (board[i][0] + board[i][1] + board[i][2] == 3)
+				return true;
+		}
+		for (int j = 0; j <= 3; j++)
+		{
+			if (board[0][j] + board[1][j] + board[2][j] == 3)
+				return true;
+		}
+		if (board[0][0] + board[1][1] + board[2][2] == 3)
+			return true;
+		if (board[0][2] + board[1][1] + board[2][0] == 3)
+			return true;
+		return false;
+
+	}
+};
