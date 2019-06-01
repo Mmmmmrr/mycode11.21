b66363ed1b212fc6207b854a26479a3a24bf87b8
Mmmmmrr <1539716914@qq.com>
2019-06-01 20:58:08 +0800

    二进制插入、日期类输出

--- oj题/oj题/test.cpp
+++ oj题/oj题/test.cpp
@@ -122,3 +122,93 @@ int main()
 		}
 	}
 }
+
+//查找组成一个偶数最接近的两个素数
+#include <iostream>
+using namespace std;
+bool Leap(int n)
+{
+	if (n == 1 || n == 2)
+		return true;
+	else
+	{
+		for (int i = 2; i < n; i++)
+		{
+			if (n%i == 0)
+				return false;
+		}
+		return true;
+	}
+}
+int main()
+{
+	int n;
+	while (cin >> n)
+	{
+		if (n % 2 != 0)
+			return 0;
+		int start = 0;
+		int end = 0;
+		for (int i = 0; i <= n / 2; i++)
+		{
+			if (Leap(i) && Leap(n - i))
+			{
+				if (i > start)
+				{
+					start = i;
+					end = n - i;
+				}
+			}
+		}
+		cout << start << "\n" << end << endl;
+	}
+
+	return 0;
+}
+
+//二进制插入
+class BinInsert {
+public:
+	int binInsert(int n, int m, int j, int i) {
+		// write code here
+		m <<= j;
+		return m | n;
+	}
+};
+//日期类
+#include <iostream>
+using namespace std;
+int a[13] = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+int main()
+{
+	int n;
+	cin >> n;
+	while (n>0)
+	{
+		int y, m, d;
+		cin >> y >> m >> d;
+		if (d == a[m] && m != 12)
+		{
+			d = 1;
+			m = m + 1;
+		}
+		else if (d == a[m] && m == 12)
+		{
+			y = y + 1;
+			d = 1;
+			m = 1;
+		}
+		else
+			d = d + 1;
+		if (m < 10)
+			cout << y << "-0" << m;
+		else
+			cout << y << "-" << m;
+		if (d < 10)
+			cout << "-0" << d << endl;
+		else
+			cout << "-" << d << endl;
+		n--;
+	}
+	return 0;
+}
