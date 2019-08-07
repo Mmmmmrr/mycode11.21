8e68f71255ab684c115ef37548ab628613b46731
Mmmmmrr <1539716914@qq.com>
2019-08-07 23:43:55 +0800

    抽象函数，final修饰不可重写

--- Virtual/Virtual/test.cpp
+++ Virtual/Virtual/test.cpp
@@ -88,3 +88,95 @@ public:
 		cout << "~Student()" << endl;
 	}
 };
+
+// 只有派生类Student的析构函数重写了Person的析构函数，下面的delete对象调用析构函数，才能构成多
+//态，才能保证p1和p2指向的对象正确的调用析构函数。
+int main()
+{
+	Person* p1 = new Person;
+	Person* p2 = new Student;
+	delete p1;
+	delete p2;
+	return 0;
+}
+
+
+//抽象类
+class Car
+{
+public:
+	virtual void Drive() = 0;
+};
+class Benz :public Car
+{
+public:
+	virtual void Drive()
+	{
+		cout << "舒适" << endl;
+	}
+};
+class BMW :public Car
+{
+public:
+	virtual void Drive()
+	{
+		cout << "宽敞" << endl;
+	}
+};
+void Test()
+{
+	Car* pBenz = new Benz;
+	pBenz->Drive();
+	Car* pBMW = new BMW;
+	pBMW->Drive();
+}
+
+class Ticket{
+public:
+	virtual void Buy()
+	{
+	}
+};
+// override 修饰派生类虚函数强制完成重写
+class Sticket :public Ticket {
+public:
+	virtual void Buy() override 
+	{
+		cout << "学生票" << endl; 
+	}
+};
+
+
+class T{
+public:
+	virtual void Ty()
+	{
+	}
+};
+// 2.override 修饰派生类虚函数强制完成重写，如果没有重写会编译报错
+class X :public T {
+public:
+	virtual void Ty() 
+	{
+		cout << "T" << endl;
+	}
+};
+
+// final 修饰基类的虚函数不能被派生类重写
+class X
+{
+public:
+	virtual void T() final 
+	{
+	}
+};
+class C :public X
+{
+public:
+	//不能被重写
+	//virtual void T() 
+	/*{ 
+		cout << "C" << endl;
+	}*/
+};
+
