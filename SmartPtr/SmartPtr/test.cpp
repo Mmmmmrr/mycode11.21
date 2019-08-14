dcccf09f97c46a08f1627f497a63a0d7f8de97a1
Mmmmmrr <1539716914@qq.com>
2019-08-14 19:59:01 +0800

    shared_ptr模拟实现

--- SmartPtr/SmartPtr/test.cpp
+++ SmartPtr/SmartPtr/test.cpp
@@ -91,6 +91,98 @@ private:
 int main()
 {
 	UniquePtr<Date> up(new Date);
-	UniquePtr<Date> copy(up);
 	return 0;
+}
+
+
+//Ä£ÄâÊµÏÖshared_ptr
+#include <thread>
+#include <mutex>
+#include <iostream>
+using namespace std;
+template<class T>
+class SharedPtr
+{
+public:
+	SharedPtr(T* ptr = nullptr)
+		:_ptr(ptr)
+		, _pRefCount(new int(1))
+		, _pMutex(new mutex)
+	{
+
+	}
+	~SharedPtr()
+	{
+		Release();
+	}
+
+	SharedPtr<T>& operator= (const SharedPtr<T>& sp)
+	{
+		if (_ptr != sp._ptr)
+		{
+			Release();
+			_ptr = sp._ptr;
+			_pRefCount = sp._pRefCount;
+			_pMutex = sp._pMutex;
+			AddRefCount();
+		}
+		return *this;
+	}
+	T& operator*()
+	{
+		return *_ptr;
+	}
+	T* operator->()
+	{
+		return _ptr;
+	}
+	int UseCount()
+	{
+		return *_pRefCount;
+	}
+	T* Get()
+	{
+		return _ptr;
+	}
+	void AddRefCount()
+	{
+		_pMutex->lock();
+		++(*_pRefCount);
+		_pMutex->unlock();
+	}
+private:
+	void Relaease()
+	{
+		bool flag = false;
+		_pMutex->lock();
+		if (--(*_pRefCount) == 0)
+		{
+			delete _ptr;
+			delete _pRefCount;
+			flag = true;
+		}
+		_pMutex->unlock();
+		if (flag == true)
+		{
+			delete _pMutex;
+		}
+	}
+
+private:
+	T* _ptr;
+	int* _pRefCount;
+	mutex* _pMutex;
+};
+int main()
+{
+	SharedPtr<int> sp1(new int(10));
+	SharedPtr<int> sp2(sp1);
+	*sp2 = 20;
+	cout << sp1.UseCount() << endl;
+	cout << sp2.UseCount() << endl;
+	SharedPtr<int> sp3(new int(10));
+	sp2 = sp3;
+	cout << sp1.UseCount() << endl;
+	cout << sp2.UseCount() << endl;
+	cout << sp3.UseCount() << endl;
 }
