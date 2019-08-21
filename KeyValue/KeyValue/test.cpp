3b31c05bc6c34f51cd04e1e60d9b0c4e8e2b70a8
Mmmmmrr <1539716914@qq.com>
2019-08-21 22:00:50 +0800

    AVL旋转

--- KeyValue/KeyValue/test.cpp
+++ KeyValue/KeyValue/test.cpp
@@ -456,9 +456,66 @@ struct AVLTreeNode
 		return true;
 	}
 
+
+	void _RotateR(PNode pParent) {
+		// pSubL: pParent的左孩子
+		// pSubLR: pParent左孩子的右孩子，注意：该
+		PNode pSubL = pParent->_pLeft;
+		PNode pSubLR = pSubL->_pRight;
+		// 旋转完成之后，30的右孩子作为双亲的左孩子
+		pParent->_pLeft = pSubLR;
+		// 如果30的左孩子的右孩子存在，更新亲双亲
+		if (pSubLR)
+			pSubLR->_pParent = pParent;
+		// 60 作为 30的右孩子
+		pSubL->_pRight = pParent;
+		// 因为60可能是棵子树，因此在更新其双亲前必须先保存60的双亲
+		PNode pPParent = pParent->_pParent;
+		// 更新60的双亲
+		pParent->_pParent = pSubL;
+		// 更新30的双亲
+		pSubL->_pParent = pPParent;
+		// 如果60是根节点，根新指向根节点的指针
+		if (NULL == pPParent)
+		{
+			_pRoot = pSubL;
+			pSubL->_pParent = NULL;
+		}
+		else
+		{
+			// 如果60是子树，可能是其双亲的左子树，也可能是右子树
+			if (pPParent->_pLeft == pParent)
+				pPParent->_pLeft = pSubL;
+			else
+				pPParent->_pRight = pSubL;
+		}
+		// 根据调整后的结构更新部分节点的平衡因子
+		pParent->_bf = pSubL->_bf = 0;
+	}
+
+	// 旋转之前，60的平衡因子可能是-1/0/1，旋转完成之后，根据情况对其他节点的平衡因子进行调整
+	void _RotateLR(PNode pParent) {
+		PNode pSubL = pParent->_pLeft;
+		PNode pSubLR = pSubL->_pRight;
+		// 旋转之前，保存pSubLR的平衡因子，旋转完成之后，需要根据该平衡因子来调整其他节点的平衡因
+		子
+			int bf = pSubLR->_bf;
+		// 先对30进行左单旋
+		_RotateL(pParent->_pLeft);
+		// 再对90进行右单旋
+		_RotateR(pParent);
+		if (1 == bf)
+			pSubL->_bf = -1;
+		else if (-1 == bf)
+			pParent->_bf = 1;
+	}
+
+
 	AVLTreeNode<T>* _pLeft; // 该节点的左孩子
 	AVLTreeNode<T>* _pRight; // 该节点的右孩子
 	AVLTreeNode<T>* _pParent; // 该节点的双亲
 	T _data;
 	int _bf; // 该节点的平衡因子
 };
+
+
